9822b50a3a91a15dba7a4fd9703f99d83a70a22c
PA2EON <eric@oosterbaan.eu>
2022-01-31 19:45:29 +0100

    Update CI-V-7300.ino

--- CI-V-7300.ino
+++ CI-V-7300.ino
@@ -12,3 +12,35 @@
 
 SoftwareSerial mySerial = SoftwareSerial(1, 2); // (RX, TX)
 
+//Now create the data and send it to the RIG
+int readCounter; // counts the number of bytes received from the radio
+
+void setup() { 
+  
+  piMode(1, INPUT);   // CI-V serial communication from IC7300
+  pinMode(2, OUTPUT); // CI-V serial communication to IC7300
+ 
+  mySerial.begin(BAUD_RATE);
+  mySerial.listen();  // only one port can be made to listen with software serial
+
+while (mySerial.available()) mySerial.read(); // clean buffer
+
+}
+
+void loop() {
+
+  mySerial.flush();
+  mySerial.write(0xFE); 
+  mySerial.write(0xFE); 
+  mySerial.write(TRX_address); 
+  mySerial.write(0xE0);
+  mySerial.write(0x03); 
+  mySerial.write(0xFD); // end sequence
+  delay(20);
+ 
+// code to read //
+  
+}
+
+
+
