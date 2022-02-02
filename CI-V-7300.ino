838013899fb6cdff99ff75ea8ace0b8308fb4936
PA2EON <eric@oosterbaan.eu>
2022-02-02 16:22:35 +0100

    Update CI-V-7300.ino

--- CI-V-7300.ino
+++ CI-V-7300.ino
@@ -1,5 +1,5 @@
 /* This small script will 'ask the IC-7300' the active frequency
-*  (c) PA2EON - Eric
+*  PA2EON - Eric
 */
 
 #include <SoftwareSerial.h> // for comms to IC7300
@@ -8,42 +8,46 @@
 #define TRX_address (0x94)  // HEX $94 = Icom IC-7300
 
 // serial connection to the Arduino UNO
-// RX = Icom radio to Arduino  : to pin 0 via resistor 4k7
-// TX = Arduino to Icom radio  : to pin 1 via diode 1N4148, with pull up 10k to Vcc (5V) on tip of 3.5 mm connector
+// RX = Icom radio to Arduino  : to pin 4 via resistor 4k7
+// TX = Arduino to Icom radio  : to pin 7 via diode 1N4148, with pull up 10k to Vcc (5V) on tip of 3.5 mm connector
 
-SoftwareSerial mySerial = SoftwareSerial(0, 1); // (RX, TX)
-//SoftwareSerial mySerial(0, 1);  // (RX, TX)
+SoftwareSerial mySerial = SoftwareSerial(4, 7); // (RX, TX)
+//SoftwareSerial mySerial(4, 7);  // (RX, TX)
 
 //Now create the data and send it to the RIG
 
 void setup() { 
- 
+
+pinMode(4, INPUT);   // CI-V serial communication from IC7300
+pinMode(7, OUTPUT); // CI-V serial communication to IC7300
 Serial.begin(BAUD_RATE);  // Dump info to serial monitor 
-mySerial.begin(BAUD_RATE);
-pinMode(0, INPUT);   // CI-V serial communication from IC7300
-pinMode(1, OUTPUT); // CI-V serial communication to IC7300
 
+ mySerial.begin(BAUD_RATE);
+mySerial.listen();  // only one port can be made to listen with software serial
 
-//mySerial.listen();  // only one port can be made to listen with software serial
-//while (mySerial.available())mySerial.read(); // clean buffer
+while (mySerial.available())mySerial.read(); // clean buffer
+
 //Serial.print("waiting for CI-V");
+
 wdt_enable(WDTO_1S); // Set WDT timer
 }
 
 void loop() {
 
-  mySerial.flush();
+  //mySerial.flush();
   mySerial.write(0xFE); 
   mySerial.write(0xFE); 
   mySerial.write(TRX_address); 
-  mySerial.write(0xE0);
-  mySerial.write(0x03); 
-  mySerial.write(0xFD); // end sequence
-   
+  mySerial.write(0xE1);
+  mySerial.write(0x03); // Read freq
+  mySerial.write(0xFD); // End sequence
+  delay(40); 
+// code to read //
+
   int stri[20];             // Receiving array of numbers (string)
  
   recvStr(stri);            // Data received from the serial, see void below
-  recvStr(stri);
+  recvStr(stri);            // No dump on screen !
 
   wdt_reset(); // Reset WDT 
 
@@ -52,11 +56,11 @@ void loop() {
    p(stri[7]);
    p(stri[6]);
    p(stri[5]); 
-  Serial.print("Mhz");
+  Serial.print(" Mhz\n");
   Serial.print(" ");
 
   delay(250);
-  
+  Serial.print("waiting for CI-V");
 }
 
 void recvStr(int *buf)
